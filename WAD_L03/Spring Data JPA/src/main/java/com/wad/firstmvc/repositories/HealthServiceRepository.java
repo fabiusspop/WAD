c7e4a50de34a10ca0a523a1b99cc465ba163b899
Fabius-Sever Pop <132004909+fabiusspop@users.noreply.github.com>
2024-04-09 11:45:21 +0300

    Delete WAD_L03

--- WAD_L03/Spring Data JPA/src/main/java/com/wad/firstmvc/repositories/HealthServiceRepository.java
+++
@@ -1,9 +0,0 @@
-package com.wad.firstmvc.repositories;
-
-import com.wad.firstmvc.domain.HealthService;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Component;
-
-@Component("healthServiceRepository")
-public interface HealthServiceRepository extends CrudRepository<HealthService, Long> {
-}
